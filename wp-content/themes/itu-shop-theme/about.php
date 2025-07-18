d4b40d56b4c745012b80a87379c16c0016bc0175
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2025-07-18 16:03:04 +0200

    Fixed footer page...?

---
+++ wp-content/themes/itu-shop-theme/about.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Template Name: About Page
+ */
+get_header(); ?>
+<div class="home-content">
+    <h1 class="about-title">About</h1>
+    <div class="about-content">
+        <p>The ITU Shop is the official store for International Telecommunication Union (ITU) publications and merchandise, offering a curated selection of standards, technical reports, and branded products to support global connectivity and innovation.</p>
+        <p>The International Telecommunication Union (ITU), a specialized agency of the United Nations, is dedicated to advancing telecommunication technologies and fostering international cooperation in the field of information and communication technologies (ICTs). Our mission is to connect the world by promoting accessible, secure, and sustainable digital solutions.</p>
+        <p><strong>Notice:</strong> This site is a prototype and not an operational store. To purchase ITU publications or merchandise, please visit the official ITU Shop at <a href="https://shop.itu.int">https://shop.itu.int</a>.</p>
+    </div>
+</div>
+<?php get_footer(); ?>
